2872c8ad06eda8f500c15fa8fcae714415b8c949
OrdinaryEnder <zairullahfreefire@outlook.com>
2023-06-13 02:36:20 +0700

    Prototype

--- asgi.py
+++ asgi.py
@@ -3,12 +3,14 @@ import asyncio
 from mangum import Mangum
 from webserver import app
 from hypercorn.config import Config
-
+from a2wsgi import ASGIMiddleware
 
 config = Config()
 config.bind = ["localhost:8080"]
 
-handler = Mangum(app)
+# vercel only supports wsgi so a2wsgi is needed here
+# handler = Mangum(app)
+handler = ASGIMiddleware(app)
 
 if __name__ == __main__:
    asyncio.run(serve(app, config))
